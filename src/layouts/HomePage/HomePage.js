2bb5c891b7b21df4e06f66617001923e28acacf8
kalapisd <kalapisd@gmail.com>
2023-10-18 17:46:52 +0200

    add promo and calendar to app

--- src/layouts/HomePage/HomePage.js
+++ src/layouts/HomePage/HomePage.js
@@ -1,11 +1,15 @@
 import ExploreTopBooks from "./components/ExploreTopBooks";
 import Carousel from "./components/Carousel";
+import SearchPromo from "./components/SearchPromo";
+import Calendar from "./components/Calendar";
 
 const HomePage = () => {
   return (
     <div>
       <ExploreTopBooks />
       <Carousel />
+      <SearchPromo/>
+      <Calendar/>
     </div>
   );
 };
