bab8083b6814d3c5be8742051d792c8831d1fe05
kalapisd <kalapisd@gmail.com>
2023-10-18 17:44:25 +0200

    add reading promo section

---
+++ src/layouts/HomePage/components/SearchPromo.js
@@ -0,0 +1,72 @@
+import readpromo from "../../../assets/images/pile-books-wooden-table.jpg";
+
+const SearchPromo = () => {
+  return (
+    <div>
+      <div className="d-none d-lg-block">
+        <div className="row g-0 mt-5">
+          <div className="col-sm-6 col-md-6">
+            <div className="col-image-left">
+              <img
+                src={`${readpromo}`}
+                class="mx-auto d-block img-thumbnail"
+                width="400"
+                alt="promo"
+              />
+            </div>
+          </div>
+          <div className="col-4 col-md-4 container d-flex justify-content-center align-items-center">
+            <div className="ml-2">
+              <h1>Discover The World of Reading</h1>
+              <p className="lead">
+                Dive into your extensive library collection for your reading
+                pleasure. Whether you're searching for adventure, knowledge, or
+                inspiration, this library offers a diverse selection of books to
+                explore. From timeless classics to contemporary bestsellers,
+                you're sure to find something captivating. Discover and lose
+                yourself in the pages of our collection today.
+              </p>
+              <a className="btn main-color btn-lg text-black" href="#">
+                Sign up
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="row g-0">
+          <div
+            className="col-4 col-md-4 container d-flex 
+                        justify-content-center align-items-center"
+          ></div>
+          <div className="col-sm-6 col-md-6">
+            <div className="col-image-right"></div>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile SearchPromo */}
+      <div className="d-lg-none">
+        <div className="container">
+          <div className="m-2">
+            <div className="col-image-left"></div>
+            <div className="mt-2">
+              <h1>Discover The World of Reading?</h1>
+              <p className="lead">
+                Dive into your extensive library collection for your reading
+                pleasure. Whether you're searching for adventure, knowledge, or
+                inspiration, this library offers a diverse selection of books to
+                explore. From timeless classics to contemporary bestsellers,
+                you're sure to find something captivating. Discover and lose
+                yourself in the pages of our collection today.
+              </p>
+              <a className="btn main-color btn-sm text-black" href="#">
+                Sign up
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchPromo;
