c11bfa489153ba28ad1b5c551541082872eebb88
kalapisd <kalapisd@gmail.com>
2023-10-20 17:26:44 +0200

    add auth and link to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,18 @@
+import { Link, NavLink } from "react-router-dom";
+import { useOktaAuth } from "@okta/okta-react";
+import { SpinnerLoading } from "../layouts/Utils/SpinnerLoading";
+
 const Navbar = () => {
+  const { oktaAuth, authState } = useOktaAuth();
+
+  if (!authState) {
+    return <SpinnerLoading />;
+  }
+
+  const handleLogout = async () => oktaAuth.signOut();
+
+  console.log(authState);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark main-color py-4">
       <div className="container-fluid">
@@ -17,24 +31,39 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                {" "}
+              <NavLink className="nav-link" to="/home">
                 Home
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                {" "}
+              <NavLink className="nav-link" to="/search">
                 Search Books
-              </a>
+              </NavLink>
             </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/calendar">
+                  Shelf
+                </NavLink>
+              </li>
+            {authState.isAuthenticated &&
+              authState.accessToken?.claims?.userType === "admin" && (
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/admin">
+                    Admin
+                  </NavLink>
+                </li>
+              )}
           </ul>
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item m-1">
-              <a type="button" className="btn btn-outline-light" href="#">
-                Sign in
-              </a>
-            </li>
+          <ul className='navbar-nav ms-auto'>
+            {!authState.isAuthenticated ?
+              <li className='nav-item m-1'>
+                <Link type='button' className='btn btn-outline-light' to='/login'>Sign in</Link>
+              </li>
+              :
+              <li>
+                <button className='btn btn-outline-light' onClick={handleLogout}>Logout</button>
+              </li>
+            }
           </ul>
         </div>
       </div>
