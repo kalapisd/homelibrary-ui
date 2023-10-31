4d85380f662b3b99b0490a2b86910b95a70bcd55
kalapisd <kalapisd@gmail.com>
2023-10-31 21:23:15 +0100

    add returnbook to carousel

--- src/layouts/HomePage/components/ReturnBook.js
+++ src/layouts/HomePage/components/ReturnBook.js
@@ -1,18 +1,33 @@
-import estaub from "../../../assets/images/EStaubsmall.jpg";
+import background from "../../../assets/images/wallpaper.jpg";
+import { StarsReview } from "../../Utils/StarsReview";
 
-const ReturnBook = () => {
+
+//TODO: find default book picture
+
+export const ReturnBook = (props) => {
+  const data = props.book.img;
+
   return (
-    <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
+    <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3 ">
       <div className="text-center">
-        <img src={`${estaub}`} width="200" alt="book" />
-        <h6 className="mt-2">Book</h6>
-        <p className="fs-6">*Rating here*</p>
-        <a className="btn btn-secondary" href="#">
+        {props.book.img ? (
+          <img
+            src={`data:image/jpeg;base64,${data}`}
+            style={{maxHeight: 200}}
+            alt="book"
+          />
+        ) : (
+          <img src={`${background}`} style={{maxWidth: 200}} alt="No book image" />
+        )}
+        <h6 className="mt-3 display-8">{props.book.title}</h6>
+        <p className="book-item mt-3">{props.book.author}</p>
+        <div className="mb-2">
+          <StarsReview rating={props.book.currentRating} size={20} />
+        </div>
+        <a className="btn btn-secondary" href="/home">
           Details
         </a>
+        </div>
       </div>
-    </div>
   );
 };
-
-export default ReturnBook;
